55f61717fcd58bf383412ee30ce795cf4d4914ec
DarielJ <DarielJ@DJ.lan>
2016-10-09 21:20:30 -0300

    Some changes to the JFrame

--- Lab5/src/Main.java
+++ Lab5/src/Main.java
@@ -1,12 +1,20 @@
 import javax.swing.JFrame;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;  									 //
+
 public class Main {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Color Grid");
+		JButton restart= new JButton( "Restart");  			//
+		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
 		myFrame.setSize(400, 400);
-
+		
+		myFrame.add(restart,BorderLayout.NORTH);
+		myFrame.setLocationRelativeTo(null); 					 //centraliza el JFrame
 		MyPanel myPanel = new MyPanel();
 		myFrame.add(myPanel);
 		MyMouseAdapter myMouseAdapter = new MyMouseAdapter();
